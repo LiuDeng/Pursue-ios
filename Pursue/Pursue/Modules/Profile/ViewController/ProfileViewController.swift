f18cd048fcff5ae42b78a3d69d07c12984591145
rain2540 <rain@APPLEdeiMac.local>
2015-03-19 08:37:57 +0800

    Revert "“我的资料”部分界面绘制"
    This reverts commit 843692289edebbeb855ef8e23c6f1c3e0956bb03.

--- Pursue/Pursue/Modules/Profile/ViewController/ProfileViewController.swift
+++ Pursue/Pursue/Modules/Profile/ViewController/ProfileViewController.swift
@@ -7,13 +7,9 @@
 //
 
 import Foundation
-import UIKit
 
-class ProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ProfileViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
-    
-    //  MARK:初始化
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -22,49 +18,10 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
-    //  MARK:生命周期
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "我的资料"
         
-        buildUI()
-        basicSetup()
-    }
-    
-    //  MARK:界面绘制
-    func buildUI() {
-        self.navigationItem.title = "我的资料"
-    }
-    
-    //  MARK:基本设置
-    func basicSetup() {
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
-    }
-    
-    //  MARK:Table View Data Source
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2
-    }
-
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-            
-        case 0:
-            return 3
-
-        default:
-            return 2
-            
-        }
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let CellIdentifier = "Cell"
-        var cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier) as? UITableViewCell
-        if cell == nil {
-            cell = UITableViewCell(style: .Default, reuseIdentifier: CellIdentifier)
-        }
-        return cell!
     }
     
 }
